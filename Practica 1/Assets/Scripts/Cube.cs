cacdee41891e40353f6e54514995333037105f66
Javier Gómez Sánchez <jagosakun27@gmail.com>
2021-09-17 17:24:09 +0200

    Ejercicio 1.a
    Este es el ejercicio de crear variables distintas

---
+++ Practica 1/Assets/Scripts/Cube.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Esto es para las variables distintas que hay que hacer
+
+    private int n = 5;
+    private float y = 25.7f;
+    private bool myBool = true;
+    private string myString = "Buenas gente";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(y);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
