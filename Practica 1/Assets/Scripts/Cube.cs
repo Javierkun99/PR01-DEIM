49cb731040bd0d029963ce25d078dd3a1140ccc8
Javier Gómez Sánchez <jagosakun27@gmail.com>
2021-09-17 20:42:33 +0200

    Ejercicio 2.c
    He combinado bucles y estructuras de control

--- Practica 1/Assets/Scripts/Cube.cs
+++ Practica 1/Assets/Scripts/Cube.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     //Contador de ciclos de while y for
-    [SerializeField] string a = "Buenos";
-    [SerializeField] string b = "dias";
+    [SerializeField] string a = "vidas";
+    [SerializeField] string b = "Tu personaje tiene";
 
     public int c = 7;
     public int d = 51;
@@ -15,14 +15,25 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        while (c<45)
+        for (c = 0; c < 100; c++)
         {
-            print(c);
-            c++;
+            if (c < 8)
+
+            {
+
+
+                print("Es mayor que 8");
+            }
+
+            else if (d >= 68)
+            {
+
+
+                print("Es igual a 68");
+            }
         }
-        
-            for (c = 7; c < 45; c++) ;
-    }
+           
+}
 
     // Update is called once per frame
     void Update()
