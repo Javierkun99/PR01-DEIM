a2f3e4edf5fb5143832dafa5b26b0d20623424d2
Javier Gómez Sánchez <jagosakun27@gmail.com>
2021-09-17 18:46:46 +0200

    Ejercicio 1.c
    Esto sería SIN contatenar

--- Practica 1/Assets/Scripts/Cube.cs
+++ Practica 1/Assets/Scripts/Cube.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    //Pongo variables de ambito publico
+    //Toca usar el "+"
    
-    public int myVar = 10;
-    public int y = 54;
-    int myResult;
+   public int num1;
+   [SerializeField] int num2;
+
+    private int resultado;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        myResult = myVar + y;
-
-        print(myResult);
+       resultado = num1 + num2;
+       print (resultado);
     }
 
     // Update is called once per frame
